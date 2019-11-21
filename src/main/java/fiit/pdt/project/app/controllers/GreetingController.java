22bec3860666e749709f354baeb208f504f3517b
Matej Sojka <matej.sojka@xit.camp>
2019-11-21 18:15:46 +0100

    new rest apis

--- src/main/java/fiit/pdt/project/app/controllers/GreetingController.java
+++ src/main/java/fiit/pdt/project/app/controllers/GreetingController.java
@@ -1,8 +1,12 @@
 package fiit.pdt.project.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,16 @@ public class GreetingController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
         return "greeting " + name;
     }
+	
+	@RequestMapping(
+		    value = "/route.json", 
+		    method = RequestMethod.POST)
+	public String process(@RequestBody(required=false) Map<String, Object> payload) {
+		if(!payload.equals(null)) {
+			System.out.println(payload.values());
+			}
+		return randomLine();
+	}
 	
 	@CrossOrigin
 	@RequestMapping(value="/data.json", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,16 +46,12 @@ public class GreetingController {
 	}
 	
 	@CrossOrigin
-	@RequestMapping("/map")
-    public String homePage() {
+	@RequestMapping(value="/category.json", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String categories() {
 		
-		DbConnector dbc = new DbConnector();
-		dbc.select();
-		
-        return "Hello world!\n";
-    }
+		return cats();
+	}
 	
-	//@RequestMapping("/supermarkets")
 	public String supermarkets() {
 		DbConnector dbc = new DbConnector();
 		String result = dbc.query("SELECT array_to_json(array_agg(row_to_json(f)))::json As feature\r\n" + 
@@ -56,23 +66,45 @@ public class GreetingController {
 	public String pois() {
 		DbConnector dbc = new DbConnector();
 		String result = dbc.query("with base as (\r\n" + 
-				"    select leisure as spec, 'leisure' as name ,st_asgeojson(way) as geo, way from planet_osm_polygon where leisure is not null\r\n" + 
+				"    select leisure as spec, 'leisure' as category ,st_asgeojson(way) as geo, way from planet_osm_polygon where leisure is not null\r\n" + 
 				"    union\r\n" + 
-				"    select \"natural\" as spec,'natural' as name ,st_asgeojson(way) as geo, way from planet_osm_polygon where \"natural\" is not null\r\n" + 
+				"    select \"natural\" as spec,'natural' as category ,st_asgeojson(way) as geo, way from planet_osm_polygon where \"natural\" is not null\r\n" + 
 				"    union\r\n" + 
-				"    select historic as spec,'historic' as name ,st_asgeojson(way) as geo, way from planet_osm_polygon where \"historic\" is not null\r\n" + 
+				"    select historic as spec,'historic' as category ,st_asgeojson(way) as geo, way from planet_osm_polygon where \"historic\" is not null\r\n" + 
 				"    union\r\n" + 
-				"    select historic as spec,'historic' as name ,st_asgeojson(way) as geo, way from planet_osm_point where \"historic\" is not null\r\n" + 
+				"    select historic as spec,'historic' as category ,st_asgeojson(way) as geo, way from planet_osm_point where \"historic\" is not null\r\n" + 
 				"    union\r\n" + 
-				"    select leisure as spec,'leisure' as name ,st_asgeojson(way) as geo, way from planet_osm_point where leisure is not null\r\n" + 
+				"    select leisure as spec,'leisure' as category ,st_asgeojson(way) as geo, way from planet_osm_point where leisure is not null\r\n" + 
 				")\r\n" + 
 				"SELECT array_to_json(array_agg(row_to_json(f)))::json As feature\r\n" + 
-				"     FROM (select d.name, d.geo::json, d.spec\r\n" + 
+				"     FROM (select d.category, d.geo::json, d.spec\r\n" + 
 				"             from planet_osm_polygon p\r\n" + 
 				"                      cross join base d\r\n" + 
 				"             where 1 = 1\r\n" + 
 				"               and p.name like '%Kamenné%'\r\n" + 
 				"               and st_dwithin(p.way::geography, d.way::geography, 1000)\r\n" + 
+				"          ) As f;");
+		return result;
+	}
+	
+	public String cats() {
+
+		return "[\"historic\",\"leisure\",\"natural\"]";
+	}
+	
+	public String randomLine() {
+		DbConnector dbc = new DbConnector();
+		String result = dbc.query("SELECT array_to_json(array_agg(row_to_json(f)))::json As feature\r\n" + 
+				"     FROM (\r\n" + 
+				"         select * from (\r\n" + 
+				"                           select name, way as geo\r\n" + 
+				"                           from planet_osm_line\r\n" + 
+				"                           union\r\n" + 
+				"                           select name, way as geo\r\n" + 
+				"                           from planet_osm_roads\r\n" + 
+				"                       ) tmp\r\n" + 
+				"        where st_length(geo::geography) > 100\r\n" + 
+				"        limit 1\r\n" + 
 				"          ) As f;");
 		return result;
 	}
