ceb0dfcc3c66e682512e6c81c3cc7869b90a8688
MatejKuzma <matejkuzma7@gmail.com>
2019-11-21 16:40:51 +0100

    Debug help fix

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -5,12 +5,87 @@ const SERVER = SERVER_ADDRESS+':'+SERVER_PORT
 
 const URL_TYPES = SERVER+"/data.json"
 
+// TEMP - MOCKUP URLs
+const URL_CATEGORIES_MOCKUP = "file:///home/whoami/SOJEC/gis-local/src/frontend/mock/categories.json"
+const URL_POIS_MOCKUP = "file:///home/whoami/SOJEC/gis-local/src/frontend/mock/pois.json"
 
-// get all atraction categories from BE
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", URL_TYPES, false ); // false for synchronous request
-xmlHttp.send( null );
+var categoriesHashMap = undefined
+var categoriesNumber = 0
 
-console.log(xmlHttp.responseText);
 
-// populate html with categeories 
+// make synchronous HTTP request to inURL
+function SynchronousHTTPRequest(inURL)
+{
+    try 
+    {
+        var xmlHttp = new XMLHttpRequest();
+
+        // false for synchronous request
+        xmlHttp.open("GET", inURL, false);
+        xmlHttp.send( null );
+        return JSON.parse(xmlHttp.responseText);
+    }
+    catch(err) 
+    {
+        alert(err);  // DEBUG HELP
+        return null;
+    }
+}
+
+// get all categories from BE
+function PopulateCategories()
+{
+    var categories = SynchronousHTTPRequest(URL_CATEGORIES_MOCKUP); // TEMP - change mockup path
+    
+    const categoryTableID = "categoryTable"
+    let categoryTable = document.getElementById(categoryTableID);
+    if(typeof(categoryTable) == 'undefined' || categoryTable == null) // DEBUG HELP
+    {
+        alert("CAN NOT FIND HTML TABLE FOR CATEGORIES!\n looking for: \"" + categoryTableID+ "\"");
+        return false;
+    }
+
+    categoriesHashMap = []
+    categoriesNumber = 0
+
+    // populate categeories
+    for( let category of categories )
+    {
+        // add new category into categoryTable
+        let newRow = categoryTable.insertRow(0);
+        let checkBoxCell = newRow.insertCell(0);
+        let categoryNameCell = newRow.insertCell(1);    
+
+        categoryNameCell.innerHTML = category;
+        var newCheckbox = document.createElement('input');
+        newCheckbox.type = "checkbox";
+        newCheckbox.checked = true
+        newCheckbox.id = categoriesNumber;
+        checkBoxCell.appendChild(newCheckbox);
+
+        // TODO - add check functionality
+
+        // add new category into category hashMap
+        categoriesHashMap[category] = {newCheckbox, objectNum : 0, objects : []};
+        categoriesNumber++;
+       
+        console.log(categoriesHashMap[category]);
+    }
+
+    return true;
+}
+
+// get all pois from BE
+function PopulateMap()
+{
+    let pois = SynchronousHTTPRequest(inURL)
+
+    // populate categeories
+    for(let pointOfInt of pois)
+    {
+        console.log(pointOfInt)
+    }
+}
+
+// get all categories from BE
+PopulateCategories();
